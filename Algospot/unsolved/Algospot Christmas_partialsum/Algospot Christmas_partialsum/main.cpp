2674ff16f56fcaebfeb1d75a608b583a14e64e06
PuppyRush <gooddaumi@naver.com>
2017-10-05 22:27:40 +0900

    update christmas matter

--- Algospot/unsolved/Algospot Christmas_partialsum/Algospot Christmas_partialsum/main.cpp
+++ Algospot/unsolved/Algospot Christmas_partialsum/Algospot Christmas_partialsum/main.cpp
@@ -1,116 +1,116 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define MAX(a,b) (((a)>(b)) ? (a) : (b))
-#define FOR(l,size) for(int l=0 ; l < size ; ++l)
-
-#include <iostream>
-#include <vector>
-#include <string.h>
-
-using namespace std;
-
-int* getPartialSum(const int*, const int);
-int christmas1(int*pSum, const int pSumSize, const int childSize);
-int christmas2(int*gifts, const int giftSize, const int childSize, int beginIdx, int untilSum, int count);
-
-int compare(const void* a, const void *b) {
-	return *(int*)a - *(int*)b;
-}
-
-int *memo;
-
-int main() {
-
-	size_t caseSize = 0;
-	scanf("%d", &caseSize);
-
-	int **result = new int*[caseSize];
-
-	for (size_t i = 0; i < caseSize; i++) {
-
-		result[i] = new int[2];
-
-		int giftSize = 0;
-		int *gifts = NULL;
-		
-		scanf("%d", &giftSize);
-		gifts = new int[giftSize];
-
-		memo = new int[giftSize];
-		memset(memo, -1, sizeof(int)*giftSize);
-
-
-		int childSize = 0;
-		scanf("%d", &childSize);
-
-		for (size_t l = 0; l < giftSize; l++) {
-			scanf("%d", &gifts[l]);
-		}
-
-		int *pSum = getPartialSum(gifts, giftSize);
-
-		result[i][0] = christmas1(pSum, giftSize, childSize) % 20091101;
-		result[i][1] = christmas2(gifts, giftSize, childSize, 0, 0,0);
-
-		delete memo;
-
-	}
-
-	for (size_t i = 0; i < caseSize; i++)
-		cout << result[i][0] << " " << result[i][1] << endl;
-
-}
-
-int christmas1(int*pSum ,const int pSumSize, const int childSize) {
-
-	if (childSize == 1)
-		return (pSumSize* (pSumSize + 1)) / 2;
-
-	int *ary = new int[childSize];
-	memset(ary, -1, sizeof(int)*childSize);
-	ary[0] = 0;
-
-	for (size_t i = 0; i < pSumSize; i++) {
-		int idx = pSum[i] % childSize;
-		ary[idx]++;
-	}
-
-	int result = 0;
-	for (size_t i = 0; i < childSize; i++)
-		if(ary[i] >0)
-			result += ary[i];
-
-	return result;
-}
-
-
-int christmas2(int*gifts, const int giftSize, const int childSize, int beginIdx, int untilSum, int count) {
-
-	if (beginIdx == giftSize)
-		return count;
-
-	untilSum += gifts[beginIdx];
-	if (untilSum%childSize==0)
-		count++;
-
-	//int ref = 0;
-	int &ref = memo[beginIdx];
-
-	ref = MAX(christmas2(gifts, giftSize, childSize, beginIdx + 1, untilSum, count), ref);
-	ref = MAX(christmas2(gifts, giftSize, childSize, beginIdx + 1, 0, 0), ref);
-
-	return ref;	
-}
-
-
-int* getPartialSum(const int* src, const int size) {
-
-	int *pSum = new int[size];
-
-	pSum[0] = src[0];
-	for (int i = 1; i < size; i++) {
-		pSum[i] = pSum[i - 1] + src[i];
-	}
-
-	return pSum;
-
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#define MAX(a,b) (((a)>(b)) ? (a) : (b))
+//#define FOR(l,size) for(int l=0 ; l < size ; ++l)
+//
+//#include <iostream>
+//#include <vector>
+//#include <string.h>
+//
+//using namespace std;
+//
+//int* getPartialSum(const int*, const int);
+//int christmas1(int*pSum, const int pSumSize, const int childSize);
+//int christmas2(int*gifts, const int giftSize, const int childSize, int beginIdx, int untilSum, int count);
+//
+//int compare(const void* a, const void *b) {
+//	return *(int*)a - *(int*)b;
+//}
+//
+//int *memo;
+//
+//int main() {
+//
+//	size_t caseSize = 0;
+//	scanf("%d", &caseSize);
+//
+//	int **result = new int*[caseSize];
+//
+//	for (size_t i = 0; i < caseSize; i++) {
+//
+//		result[i] = new int[2];
+//
+//		int giftSize = 0;
+//		int *gifts = NULL;
+//		
+//		scanf("%d", &giftSize);
+//		gifts = new int[giftSize];
+//
+//		memo = new int[giftSize];
+//		memset(memo, -1, sizeof(int)*giftSize);
+//
+//
+//		int childSize = 0;
+//		scanf("%d", &childSize);
+//
+//		for (size_t l = 0; l < giftSize; l++) {
+//			scanf("%d", &gifts[l]);
+//		}
+//
+//		int *pSum = getPartialSum(gifts, giftSize);
+//
+//		result[i][0] = christmas1(pSum, giftSize, childSize) % 20091101;
+//		result[i][1] = christmas2(gifts, giftSize, childSize, 0, 0,0);
+//
+//		delete memo;
+//
+//	}
+//
+//	for (size_t i = 0; i < caseSize; i++)
+//		cout << result[i][0] << " " << result[i][1] << endl;
+//
+//}
+//
+//int christmas1(int*pSum ,const int pSumSize, const int childSize) {
+//
+//	if (childSize == 1)
+//		return (pSumSize* (pSumSize + 1)) / 2;
+//
+//	int *ary = new int[childSize];
+//	memset(ary, -1, sizeof(int)*childSize);
+//	ary[0] = 0;
+//
+//	for (size_t i = 0; i < pSumSize; i++) {
+//		int idx = pSum[i] % childSize;
+//		ary[idx]++;
+//	}
+//
+//	int result = 0;
+//	for (size_t i = 0; i < childSize; i++)
+//		if(ary[i] >0)
+//			result += ary[i];
+//
+//	return result;
+//}
+//
+//
+//int christmas2(int*gifts, const int giftSize, const int childSize, int beginIdx, int untilSum, int count) {
+//
+//	if (beginIdx == giftSize)
+//		return count;
+//
+//	untilSum += gifts[beginIdx];
+//	if (untilSum%childSize==0)
+//		count++;
+//
+//	//int ref = 0;
+//	int &ref = memo[beginIdx];
+//
+//	ref = MAX(christmas2(gifts, giftSize, childSize, beginIdx + 1, untilSum, count), ref);
+//	ref = MAX(christmas2(gifts, giftSize, childSize, beginIdx + 1, 0, 0), ref);
+//
+//	return ref;	
+//}
+//
+//
+//int* getPartialSum(const int* src, const int size) {
+//
+//	int *pSum = new int[size];
+//
+//	pSum[0] = src[0];
+//	for (int i = 1; i < size; i++) {
+//		pSum[i] = pSum[i - 1] + src[i];
+//	}
+//
+//	return pSum;
+//
+//}
--- Algospot/unsolved/Algospot Christmas_partialsum/Algospot Christmas_partialsum/new_main.cpp
+++ Algospot/unsolved/Algospot Christmas_partialsum/Algospot Christmas_partialsum/new_main.cpp
@@ -1,16 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define MIN(a,b) (a>b?b:a)
 
 #define ASCII_BEGIN_ZERO 33
 #define ASCII_COUNT_ALPHABET 26
 #define ASCII_BEGIN_a	 97
 #define ASCII_BEGIN_A	 65
 
+#undef INFINITY
+#define INFINITY INT32_MIN
+
 #include <iostream>
 #include <cstring>
 #include <cmath>
 #include <algorithm>
 #include <vector>
+#include <map>
 #include <bitset>
 #include <algorithm>
 #include <functional>   // std::multiplies
@@ -34,19 +37,14 @@ typedef std::vector<string> VSTR;
 typedef std::vector<std::vector<char>> V2CHAR;
 typedef std::vector<char> VCHAR;
 
+#define EMPTY -1
 
+int boxes;
+int mans;
+vector<int> dolls;
+V2INT memo;
+vector<int> sums;
 
-int one(int boxes, int mans, vector<int> dolls) {
-
-
-
-}
-
-int two(int boxes, int mans, vector<int> dolls) {
-
-
-
-}
 
 vector<int> GetSum(vector<int> vec) {
 
@@ -59,28 +57,74 @@ vector<int> GetSum(vector<int> vec) {
 
 }
 
-int GetPartialSum(int i, int j) {
+int GetPartialSum(int from, int to) {
 
+	if (from == to)
+		return dolls[from];
+	else if (from == 0)
+		return sums[to];
 
-
+	return sums[to]-sums[from-1];
 }
 
-vector<int> sums;
+
+int one() {
+
+	int count = 0;
+	for (int to = 0; to < boxes; to++) {
+		for (int from = 0; from <= to; from++)
+			if ( GetPartialSum(from, to) % mans == 0) {
+				count++;
+				cout << "from:" << from << "to:" << to << endl;
+			}
+	}
+	return count;
+}
+
+int two(const int from, const int to, int count) {
+
+	if (from >= boxes || to >= boxes) {
+		return count;
+	}
+	if (from > to)
+		return count;
+
+	int val = INFINITY;
+	if (memo[from][to] != EMPTY)
+		return count;
+	else {
+		val = memo[from][to] = GetPartialSum(from, to);
+	}
+
+	if (val%mans == 0) {
+		count++;
+		cout << "from:" << from << "to:" << to << endl;
+	}
+
+	return max(two(from, to + 1, count), two(from + 1, to, count));
+}
+
 
 int main() {
 	int caseSize = 0;
 	scanf_s("%d", &caseSize);
 	
 	for (int i = 0; i < caseSize; i++) {
-		int boxes = 0, mans = 0;
-		vector<int> dolls(boxes);
+		
 		scanf_s("%d %d", &boxes, &mans);
+		vector<int> _dolls(boxes);
 		for (int i = 0; i < boxes; i++) {
-			scanf_s("%d ", &dolls[i]);
+			scanf_s("%d ", &_dolls[i]);
 		}
+		memo = V2INT(boxes, VINT(boxes, EMPTY));
+		dolls = _dolls;
 		sums = GetSum(dolls);
-		int a = one(boxes, mans, dolls);
-		int b = two(boxes, mans, dolls);
+
+		int a = one();
+		cout << "----" << endl;
+		int b = two(0, 0, 0);
+		cout << "----" << endl;
+		cout << a << " " <<b << endl;
 	}
 
 }
