59e43a7312419dcbeab9eb1e987a142befb1ed1c
PuppyRush <gooddaumi@naver.com>
2018-09-28 23:19:15 +0900

    update

--- ICPC/2016_asia_reginal_problemL/2016_asia_reginal_problemL/Source.cpp
+++ ICPC/2016_asia_reginal_problemL/2016_asia_reginal_problemL/Source.cpp
@@ -8,46 +8,85 @@ typedef struct truck
 {
 	int weight;
 	int which;
+	truck(int we, int wh)
+		:weight(we), which(wh)
+	{}
+};
+
+typedef struct bridge
+{
+	deque<truck> in;
+	queue<truck> wait;
+	size_t time;
+	size_t now_weight;
+	const size_t weight;
+	const size_t length;
+	bool going;
+
+	bridge(int bridge_length, int weight, vector<int>& truck_weights)
+		:length(bridge_length),weight(weight),time(1),now_weight(0),going(false)
+	{
+		for (auto t : truck_weights)
+			wait.push(truck(t, 0));
+	}
+
+	bool isFull()
+	{
+		return in.size() == length;
+	}
+	
+	bool tooWeight()
+	{
+		return now_weight + wait.front().weight > weight;
+	}
+
+	void go()
+	{
+		if (wait.empty())
+			return;
+
+		if (!isFull() && !tooWeight())
+		{
+			in.push_back(truck(wait.front()));
+			now_weight += wait.front().weight;
+			wait.pop();
+		}
+	}
+
+	void ahead()
+	{
+		for (auto it = in.begin(); it != in.end(); it++)
+			it->which++;
+
+		while( !in.empty())
+		{
+			if (in.front().which == length)
+			{
+				now_weight -= in.front().weight;
+				in.pop_front();
+				continue;
+			}
+			else
+				break;
+		}
+		time++;
+	}
+
+	bool finish()
+	{
+		return wait.empty() && in.empty();
+	}
 };
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
 	int answer = 0;
 
-	queue<truck> q;
-	for (auto t : truck_weights)
+	bridge b(bridge_length, weight, truck_weights);
+
+	while (!b.finish())
 	{
-		truck _t;
-		_t.weight = t;
-		_t.which = -1;
-		q.push(_t);
-	}
-
-	deque<truck> b;
-	int t_weight = 0;
-	for(;; answer++)
-	{
-		for (auto it = b.begin(); it != b.end(); it++)
-			it->which++;
-
-		if (!q.empty() && b.size() < bridge_length && t_weight+q.front().weight <= weight)
-		{
-			b.push_back(q.front());
-			t_weight += q.front().weight;
-			q.pop();
-			continue;
-		}
-
-		if (!b.empty())
-		{
-			if (b.front().which == bridge_length-1)
-			{
-				t_weight -= b.front().weight;
-				b.pop_front();
-			}
-		}
-
-		if(b.empty() && q.empty())
-			break;
+		b.go();
+		b.ahead();
 	}
 
 	return answer;
@@ -56,13 +95,22 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
 int main()
 {
 	vector<int> a;
-	a.push_back(7);
+	/*a.push_back(7);
 	a.push_back(4);
 	a.push_back(5);
 	a.push_back(6);
-	solution(2, 10, a);
+	solution(2, 10, a);*/
 
-	/*a.push_back(10);
-	solution(100, 100, a);*/
+	a.push_back(10);
+	//a.push_back(10);
+	//a.push_back(10);
+	//a.push_back(10);
+	//a.push_back(10);
+	//a.push_back(10);
+	//a.push_back(10);
+	//a.push_back(10);
+	//a.push_back(10);
+	//a.push_back(10);
+	solution(100, 100, a);
 }
 
---
+++ Programmers/dynamic_shortestway/dynamic_shortestway/Source.cpp
@@ -0,0 +1,74 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool visit[100][100];
+bool map[100][100];
+int width;
+int height;
+int answer;
+
+int order[4][2] =
+{ { 0,1 },
+{1,0},
+{-1,0},
+{0,-1} };
+
+void find(const int row, const int col, const int pre_row, const int pre_col)
+{
+	if (row == height && col == width )
+	{
+		answer++;
+		return;
+	}
+	else if (row == height+1 || col == width+1 || row == 0 || col == 0)
+		return;
+	//else if (visit[row][col])
+	//	return;
+	else if (map[row][col])
+		return;
+
+	//visit[row][col] = true;
+
+	for (int i = 0; i < 4; i++)
+	{
+		const int next_row = row + order[i][0];
+		const int next_col = col + order[i][1];
+		if (pre_row == next_row && pre_col == next_col)
+			continue;
+
+		find(next_row, next_col, row,col );
+	}
+
+}
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+	answer = 0;
+	height = n;
+	width = m;
+	
+	memset(&map, 0, width*height);
+	memset(&visit, 0, width*height);
+	
+
+	for (int i=0 ; i < puddles.size() ; i++)
+	{
+		for (int l = 0; l < puddles.at(i).size(); l++)
+			map[i][l] = true;
+	}
+
+	find(1,1,-1,-1);
+
+	return answer;
+}
+
+int main()
+{
+	vector<vector<int>> v;
+	for (int i = 0; i < 2; i++)
+		v.push_back(vector<int>());
+
+	v[1].push_back(2);
+	solution(4, 3, v);
+}
